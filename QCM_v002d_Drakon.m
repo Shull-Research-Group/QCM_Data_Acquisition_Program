8c2809087fc244c6029a4c54bf020eddb6264e12
Shull-Research-Group <Shull.Research.Group.@gmail.com>
2016-05-06 15:49:36 -0500

    udpate

--- QCM_v002d_Drakon.m
+++ QCM_v002d_Drakon.m
@@ -1,6 +1,21 @@
 function varargout = QCM_v002d_Drakon(varargin)
-%This QCM Program was created by Chyi-Huey Joshua Yeh from the Shull
-%Research Group at Northwestern University.
+%
+% Copyright (C) 2016 Joshua Yeh (Shull Research Group, Northwestern Uni.)
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>
+%
+%
 % QCM_V002D_DRAKON MATLAB code for QCM_v002d_Drakon.fig
 %      QCM_V002D_DRAKON, by itself, creates a new QCM_V002D_DRAKON or raises the existing
 %      singleton*.
---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+A working version of the QCM manual can be found in the folder, "QCM MATLAB manual".
+
+The MATLAB script was originally created by Joshua Yeh for the Shull Research Group.
+The AccessMyVNA and myVNA programs were obtained from http://g8kbb.co.uk/html/downloads.html.
+Creation and full credit of the myVNA program is given to Dave, G8KBB.
+Most of the creation and credit is given to Dave, G8KBB for the AccessMyVNA program.
+AccessMyVNA was modified by Joshua Yeh to suit the purpose for this QCM Data Acquisition program.
+
+This MATLAB script and program is to be used for ACADEMIC PURPOSES ONLY.
